db430cffdc60d35c68fcd514017d61511b66d0f8
Mohammad Tauchid <muhammadtauchid@gmail.com>
2022-11-03 11:27:45 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+class User {
+
+    constructor() {
+        this.message = 'Hello World';
+        console.log(this.message);
+    }
+
+    getUserName() {
+
+    }
+
+    getUserStatus() {
+    
+    }
+
+}
+
+module.exports = User;
